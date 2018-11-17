baf13e4b01d1e7cdc1cbc8e87eeba631ab0a270a
Danko Petrovic <dp24031995@gmail.com>
2018-11-17 19:08:14 +0100

    basic form

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { Formik } from 'formik';
+import { Container, Form, Button } from 'semantic-ui-react';
 
 function Login() {
-  return (
-    <form>
-      <input type='text' placeholder='Name' />
-      <input type='email' placeholder='Email' />
-      <button type='submit'>Submit</button>
-    </form>
-  );
+  return <BasicForm />;
 }
+
+const BasicForm = () => (
+  <Formik
+    initialValues={{ name: '', email: '' }}
+    onSubmit={(values, actions) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        actions.setSubmitting(false);
+      }, 1000);
+    }}
+    render={props => (
+      <Container>
+        <Form onSubmit={props.handleSubmit}>
+          <Form.Input
+            type='text'
+            placeholder='Name'
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.name}
+            name='name'
+          />
+
+          <Form.Input
+            type='text'
+            placeholder='Email'
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+            value={props.values.email}
+            name='email'
+          />
+          {props.errors.name && <div id='feedback'>{props.errors.name}</div>}
+          <Button primary type='submit'>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    )}
+  />
+);
 
 export default Login;
