bc56697bfc641aef9501e5d3995cf6574d2ba1ac
Danko Petrovic <dp24031995@gmail.com>
2018-11-27 17:12:51 +0100

    set defaults on axios instance instead

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '../config';
 
-const instance = axios.create({
+export const instance = axios.create({
   baseURL: config.BASE_URL,
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
--- client/src/utils/setAuthorizationHeader.js
+++ client/src/utils/setAuthorizationHeader.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import { instance } from './api';
 
 export default function(token = null) {
   if (token) {
-    axios.defaults.headers.common.authorization = token;
+    instance.defaults.headers.common['Authorization'] = token;
   } else {
-    delete axios.defaults.headers.common.authorization;
+    instance.defaults.headers.common['Authorization'] = null;
   }
 }
